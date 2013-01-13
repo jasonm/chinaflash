e14c6b4418ba750aa11a4e715a5c7e72ae6f1bbc
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-12 20:30:52 -0800

    Add db seed for student,section for now (later sync down from api)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Student.delete_all
+Section.delete_all
+
+Section.create({
+  name: 'Chinese 101',
+  state_id: 1001,
+  students: [
+    Student.create(first_name: 'Jason', last_name: 'Morrison', state_id: 2001),
+    Student.create(first_name: 'John',  last_name: 'Cumbers',  state_id: 2002)
+  ]
+})
+
+Section.create({
+  name: 'Chinese 202',
+  state_id: 1002,
+  students: [
+    Student.create(first_name: 'Amy',   last_name: 'Appleton',  state_id: 2003),
+    Student.create(first_name: 'Billy', last_name: 'Bobberton', state_id: 2004)
+  ]
+})
