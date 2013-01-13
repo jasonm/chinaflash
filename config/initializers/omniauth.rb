8f455100432e1f05af2ba3b9fb39ac3bb109557c
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-12 21:03:50 -0800

    never use production SLC

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
-    provider :slc, ENV['SLC_KEY'], ENV['SLC_SECRET'], :model => Teacher
-  else
-    provider :slc, ENV['SLC_KEY'], ENV['SLC_SECRET'], :model => Teacher, :setup => lambda{|env| 
-       env['omniauth.strategy'].options[:client_options].site = 'https://api.sandbox.slcedu.org'
-    }
-  end
+  provider :slc, ENV['SLC_KEY'], ENV['SLC_SECRET'], :model => Teacher, :setup => lambda{|env| 
+    env['omniauth.strategy'].options[:client_options].site = 'https://api.sandbox.slcedu.org'
+  }
 end
