5cac43e3daf2141905e55f03b106eea8495248eb
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-12 19:40:45 -0800

    Base styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SlcSample2::Application.routes.draw do
+  # initial from slc-example
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
-  root to: "students#index"
+
+  # role selection and login
+  resource :role_selection, only: %w(show)
+  root to: "role_selection#show"
 end
