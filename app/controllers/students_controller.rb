396e2ad033712178843b31139585cbc4a8fd8f11
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-12 19:20:34 -0800

    No real users yet

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,10 @@
 class StudentsController < ApplicationController
-  
   def index
-    def slc_request
-      if current_user
-        path = 'https://api.sandbox.slcedu.org/api/rest/v1/students'
-        headers = { "Accept" => 'application/vnd.slc+json',
-                      "Content-Type" => 'application/vnd.slc+json',
-                      "Authorization" => "bearer #{session[:token]}" }
+    path = 'https://api.sandbox.slcedu.org/api/rest/v1/students'
+    headers = { "Accept" => 'application/vnd.slc+json',
+      "Content-Type" => 'application/vnd.slc+json',
+      "Authorization" => "bearer #{session[:token]}" }
 
-        @slc_request = HTTParty.send(request.method.underscore.to_sym, path, :headers => headers)
-      end
-    end
+    @slc_request = HTTParty.send(request.method.underscore.to_sym, path, :headers => headers)
   end
-  
 end
